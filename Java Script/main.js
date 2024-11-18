50cfdd53c6d705280c3e886fd7313f95048e1a4e
MyaBurns <myab827@icloud.com>
2024-11-17 18:13:20 -0800

    added date times and greetings

--- Java Script/main.js
+++ Java Script/main.js
@@ -1,6 +1,14 @@
 console.log("Hello, World");
 var copyright = document.getElementById("copyright")
 const d = new Date();
+let hour=d.getHours();
 let year = d.getFullYear();
 copyright.innerHTML+=d
 console.log(d);
+if(hour < 12){
+    console.log ("Good morning!")
+} else if(hour > 12 && hour < 17) {
+    console.log ("Good afternoon!")
+} else {
+    console.log ("Good evening!")
+};
